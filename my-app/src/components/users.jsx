3f221e606fa40f35465386d430a580fe7cf5dc5c
Alexander Shishkin <alexander.sh73@gmail.com>
2022-12-21 14:06:43 +0400

    Home work

--- my-app/src/components/users.jsx
+++ my-app/src/components/users.jsx
@@ -1,12 +1,8 @@
 import React ,{useState} from "react";
 import API from "../API";
 
-
-
 const Users =()=>{
     const [users , setUsers] = useState(API.users.fetchAll())
     console.log( users)
- 
-return <h1>User1</h1>
 }
 export default Users
